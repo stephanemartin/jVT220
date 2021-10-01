4649faae9e48f2b09a8d1e8fe52f22ebf1cb22cd
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2021-10-01 01:30:58 -0300

    release 1.3.2

--- src/main/java/nl/lxtreme/jvt220/terminal/TerminalClient.java
+++ src/main/java/nl/lxtreme/jvt220/terminal/TerminalClient.java
@@ -5,81 +5,95 @@ import java.awt.Point;
 import java.io.IOException;
 import java.util.Optional;
 import javax.net.SocketFactory;
+
 import nl.lxtreme.jvt220.terminal.vt220.VT220Terminal;
 import org.apache.commons.net.telnet.InvalidTelnetOptionException;
 import org.apache.commons.net.telnet.VT420Client;
 
+import static nl.lxtreme.jvt220.terminal.vt220.VT220Parser.BELL;
+
 public class TerminalClient {
 
-  private final VT420Client client;
-  private final VT220Terminal terminal;
-  private final SwingFrontendProxy swingFrontendProxy;
-  private final ConnectionListenerBroadcast connectionListenerBroadcast;
+    private final VT420Client client;
+    private final VT220Terminal terminal;
+    private final SwingFrontendProxy swingFrontendProxy;
+    private final ConnectionListenerBroadcast connectionListenerBroadcast;
 
-  public TerminalClient(Dimension screenSize, String terminalType) {
-    this.terminal = new VT220Terminal(screenSize.width, screenSize.height);
-    client = new VT420Client(terminalType);
-    connectionListenerBroadcast = new ConnectionListenerBroadcast();
-    swingFrontendProxy = new SwingFrontendProxy();
-    swingFrontendProxy.setTerminal(terminal);
-    swingFrontendProxy.setConnectionListener(connectionListenerBroadcast);
-    terminal.setFrontend(swingFrontendProxy);
-  }
 
-  public void connect(String address, int port, int timeout)
-      throws ConnectionException {
-    try {
-      client.setupOptionHandlers();
-      client.setConnectTimeout(timeout);
-      client.connect(address, port);
-      connectionListenerBroadcast.onConnection();
-      terminal.getFrontend().connect(client.getInputStream(), client.getOutputStream());
-    } catch (InvalidTelnetOptionException | IOException e) {
-      throw new ConnectionException(e, String.format("%s:%s", address, port));
+    public TerminalClient(Dimension screenSize, String terminalType) {
+        this.terminal = new VT220Terminal(screenSize.width, screenSize.height);
+        client = new VT420Client(terminalType);
+        connectionListenerBroadcast = new ConnectionListenerBroadcast();
+        swingFrontendProxy = new SwingFrontendProxy();
+        swingFrontendProxy.setTerminal(terminal);
+        swingFrontendProxy.setConnectionListener(connectionListenerBroadcast);
+        terminal.setFrontend(swingFrontendProxy);
     }
-  }
 
-  public void disconnect() throws IOException {
-    client.disconnect();
-    swingFrontendProxy.disconnect();
-  }
+    public void connect(String address, int port, int timeout)
+            throws ConnectionException {
+        try {
+            client.setupOptionHandlers();
+            client.setConnectTimeout(timeout);
+            client.connect(address, port);
+            connectionListenerBroadcast.onConnection();
+            terminal.getFrontend().connect(client.getInputStream(), client.getOutputStream());
+        } catch (InvalidTelnetOptionException | IOException e) {
+            throw new ConnectionException(e, String.format("%s:%s", address, port));
+        }
+    }
 
-  public void sendTextByCurrentCursorPosition(String text) throws IOException {
-    terminal.write(text);
-  }
+    public void disconnect() throws IOException {
+        client.disconnect();
+        swingFrontendProxy.disconnect();
+    }
 
-  public String getScreen() {
-    return terminal.toString();
-  }
+    public void sendTextByCurrentCursorPosition(String text) throws IOException {
+        terminal.write(text);
+    }
 
-  public void addScreenChangeListener(ScreenChangeListener listener) {
-    swingFrontendProxy.addScreenChangeListener(listener);
-  }
+    public String getScreen() {
+        return terminal.toString();
+    }
 
-  public void removeScreenChangeListener(ScreenChangeListener listener) {
-    swingFrontendProxy.removeScreenChangeListener(listener);
-  }
+    public void addScreenChangeListener(ScreenChangeListener listener) {
+        swingFrontendProxy.addScreenChangeListener(listener);
+    }
 
-  public Optional<Point> getCursorPosition() {
-    ICursor cursor = terminal.getCursor();
-    return cursor.isVisible() ? Optional.of(new Point(cursor.getX(), cursor.getY()))
-        : Optional.empty();
-  }
+    public void removeScreenChangeListener(ScreenChangeListener listener) {
+        swingFrontendProxy.removeScreenChangeListener(listener);
+    }
 
-  public Dimension getScreenSize() {
-    return new Dimension(terminal.getWidth(), terminal.getHeight());
-  }
+    public Optional<Point> getCursorPosition() {
+        ICursor cursor = terminal.getCursor();
+        return cursor.isVisible() ? Optional.of(new Point(cursor.getX(), cursor.getY()))
+                : Optional.empty();
+    }
 
-  public void addConnectionListener(ConnectionListener listener) {
-    connectionListenerBroadcast.add(listener);
-  }
- 
-  public void removeConnectionListener(ConnectionListener listener) {
-    connectionListenerBroadcast.remove(listener);
-  }
+    public Dimension getScreenSize() {
+        return new Dimension(terminal.getWidth(), terminal.getHeight());
+    }
 
-  public void setSocketFactory(SocketFactory socketFactory) {
-    client.setSocketFactory(socketFactory);
-  }
+    public void addConnectionListener(ConnectionListener listener) {
+        connectionListenerBroadcast.add(listener);
+    }
 
+    public void removeConnectionListener(ConnectionListener listener) {
+        connectionListenerBroadcast.remove(listener);
+    }
+
+    public void setSocketFactory(SocketFactory socketFactory) {
+        client.setSocketFactory(socketFactory);
+    }
+
+    public boolean isAlarmOn() {
+        return terminal.isAlarmSounded();
+    }
+
+    public boolean resetAlarm() {
+        boolean ret = terminal.isAlarmSounded();
+        terminal.setAlarmSounded(false);
+        return ret;
+    }
 }
+
